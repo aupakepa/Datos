df1fd421f8295bb2694c1ff497d7662cfc744c04
Javier M <jmonforte@fontecabras.es>
2018-10-04 18:38:57 +0200

    first commit

---
+++ Unidad1.8/src/E11.java
@@ -0,0 +1,14 @@
+/*EJERCICIO 11
+Escribe un programa Java que pida al usuario los datos de una serie de médicos 
+y que los guarde en un fichero binario. El usuario introducirá datos de medicos hasta que indique 
+que no quiere añadir ninguno más. Los médicos se almacenarán en un fichero binario como objetos de una clase Medico que deberá contener nombre,
+ sueldo, año de nacimiento y antigüedad*/
+public class E11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
